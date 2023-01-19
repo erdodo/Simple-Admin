1aaae9b01b55a7e8c870b47d451bf8748d7359da
erdodo <erdoganyesil3@gmail.com>
2023-01-20 00:50:53 +0300

    new-simple-admin

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app-body">
+    <div class="">
+      <el-container>
+        <el-aside width="auto"><Aside></Aside></el-aside>
+        <el-container class="w-100">
+          <el-header><Header></Header></el-header>
+          <el-main v-if="true">
+            <div v-for="i in 100" :key="i" class="h1">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam molestias, unde, beatae et consequuntur
+              eligendi odio aperiam ut eos dolore praesentium sunt delectus nesciunt facere! Atque, error? Modi, voluptate
+              dolorum.
+            </div>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+        <el-aside width="auto">Right side</el-aside>
+      </el-container>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Aside from "@/layout/Aside.vue";
+import Header from "./layout/Header.vue";
+export default {
+  components: { Aside, Header },
+};
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
---
+++ src/Page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="position-relative">
+    <app v-if="true"></app>
+  </div>
+</template>
+
+<script>
+import app from "@/app.vue";
+export default {
+  components: { app },
+};
+</script>
+
+<style></style>
